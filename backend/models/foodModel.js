61eedc05c565ffb8a40223cd7fd31a52eab9887d
Rajeev <ronaldocrcr5@gmail.com>
2024-05-03 15:56:40 +0530

    created the food model

---
+++ backend/models/foodModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+// Create the Schema for describing the food model properties.
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  desciption: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+});
+
+// Create the Model
+const foodModel = mongoose.models.food || mongoose.model("food", foodSchema);
+
+export default foodModel;
