1e1408a07669b0b82de21b8b7442c02da5ace8d6
Rajeev <ronaldocrcr5@gmail.com>
2024-05-04 20:31:00 +0530

    Successfully sent data from admin frontend to backend db

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-options">
-        <div className="sidebar-option">
+        <NavLink to={"/add"} className="sidebar-option">
           <img src={assets.add_icon} alt="" />
           <p>Add Items</p>
-        </div>
+        </NavLink>
 
-        <div className="sidebar-option">
+        <NavLink to={"/list"} className="sidebar-option">
           <img src={assets.order_icon} alt="" />
           <p>List Items</p>
-        </div>
+        </NavLink>
 
-        <div className="sidebar-option">
+        <NavLink to={"/orders"} className="sidebar-option">
           <img src={assets.order_icon} alt="" />
           <p>Orders</p>
-        </div>
+        </NavLink>
       </div>
     </div>
   );
