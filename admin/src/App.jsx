b4a39b0e2ff0c44f96015eb49fd92f5ecb00cbda
Rajeev <ronaldocrcr5@gmail.com>
2024-05-06 22:05:49 +0530

    Sent url as a props from app to add, list and order pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -20,9 +20,9 @@ const App = () => {
         <Sidebar />
 
         <Routes>
-          <Route path="/add" element={<Add />} />
-          <Route path="/list" element={<List />} />
-          <Route path="/orders" element={<Orders />} />
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
       </div>
     </div>
--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -4,8 +4,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
 
-const List = () => {
-  const url = "http://localhost:4000";
+const List = ({ url }) => {
   const [list, setList] = useState([]);
 
   const fetchList = async () => {
