d8cd7cf892dc7187b64eed66398d32b78704e7d6
Rajeev <ronaldocrcr5@gmail.com>
2024-05-08 15:33:41 +0530

    shown image from db in the cart page also

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   // Access cart items, food list, remove from cart functionality from our context.
-  const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount, url } =
     useContext(StoreContext);
 
   const navigate = useNavigate();
@@ -32,7 +32,7 @@ const Cart = () => {
               return (
                 <div>
                   <div className="cart-items-title cart-items-item">
-                    <img src={item.image} alt="" />
+                    <img src={url + "/images/" + item.image} alt="" />
                     <p>{item.name}</p>
                     <p>${item.price}</p>
                     <p>{cartItems[item._id]}</p> {/*Quantity*/}
