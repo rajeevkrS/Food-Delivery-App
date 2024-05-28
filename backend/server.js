e376012d04e5c3d1818ceae9fc1e01d9144dcac5
Rajeev <ronaldocrcr5@gmail.com>
2024-05-28 15:51:33 +0530

    deploy test 1 using vercel

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,16 @@ const app = express();
 const port = 4000;
 
 //middleware
-app.use(express.json());
 app.use(cors());
+app.use(
+  cors({
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 
 //DB connection
 connectDB();
