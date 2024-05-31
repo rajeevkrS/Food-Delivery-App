6d4141251f980bfa83866647ca12bf14fc368bdd
Rajeev <ronaldocrcr5@gmail.com>
2024-05-31 18:32:07 +0530

    deploy test 6

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://rajeevkumar:789123@cluster0.k76semo.mongodb.net/food-delivery-app"
+      "mongodb+srv://rajeevkumar:789123@cluster0.k76semo.mongodb.net/food-delivery-app?retryWrites=true&w=majority"
     )
     .then(() => console.log("DB connected"));
 };
