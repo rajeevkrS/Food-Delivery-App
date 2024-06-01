e5f45d99d7a9fb3d6dc8eda9231d33e3b3bad6cc
Rajeev <ronaldocrcr5@gmail.com>
2024-06-01 16:15:32 +0530

    deploy test 6

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState({});
   // const url = "http://localhost:4000";
-  const url = "https://merneats-api.vercel.app/";
+  const url = "https://merneats-api.vercel.app";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
