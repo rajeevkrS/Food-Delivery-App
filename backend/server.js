51e368c4bee8b814c539f4b9124feae20a4b01e3
Rajeev <ronaldocrcr5@gmail.com>
2024-06-13 20:48:13 +0530

    deploy test 8

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import orderRouter from "./routes/orderRoute.js";
 
 //app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 5000;
 
 //middlewaree
 // app.use(cors());
