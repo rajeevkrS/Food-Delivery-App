979176abcdec3ad4c5168f646550860069965047
Rajeev <ronaldocrcr5@gmail.com>
2024-06-22 21:05:11 +0530

    added some styling in Place order page

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -16,5 +16,5 @@ export const assets = {
   all_order_items,
 };
 
-// export const url = "http://localhost:4000";
+// export const url = "http://localhost:8000";
 export const url = "https://food-delivery-app-backend-enj0.onrender.com";
