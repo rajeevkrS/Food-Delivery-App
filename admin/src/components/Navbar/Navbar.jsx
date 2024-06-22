df720b529f8c0d37ec15eca74a3eff1400301397
Rajeev <ronaldocrcr5@gmail.com>
2024-06-22 20:24:49 +0530

    added home navigation to the logo

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -6,8 +6,10 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div>
-        <h2 className="logo">MernEats.com</h2>
-        <span className="admin">Admin Panel</span>
+        <a href="/">
+          <h2 className="logo">MernEats.com</h2>
+          <span className="admin">Admin Panel</span>
+        </a>
       </div>
       <img className="profile" src={assets.profile_image} alt="" />
     </div>
