50f6321be0809f01136986088e52aa781e789255
Rajeev <ronaldocrcr5@gmail.com>
2024-07-13 14:27:10 +0530

    vercel deploy test 1

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  // const url = "http://localhost:8000";
+  // const url = "http://localhost:800";
   const url = import.meta.env.VITE_APP_BACKEND_URL;
 
   // axios.defaults.baseURL = ;
