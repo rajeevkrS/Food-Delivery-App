8cc5c9d0ebab3a713f58cd0ae333707963be9ae2
Rajeev <ronaldocrcr5@gmail.com>
2024-09-30 17:00:39 +0530

    cron job set to 1hrs

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const URL = process.env.BACKEND_URL;
 
-const job = new cron.CronJob("*/14 * * * *", function () {
+const job = new cron.CronJob("0 * * * *", function () {
   const protocol = URL.startsWith("https") ? https : http; // Choose the protocol
   protocol
     .get(URL, (res) => {
@@ -22,3 +22,19 @@ const job = new cron.CronJob("*/14 * * * *", function () {
 });
 
 export default job;
+
+// CRON JOB EXPLANATION:
+// Cron jobs are scheduled tasks that run periodically at fixed intervals or specific times
+// send 1 GET request for every 14 minutes
+
+// Schedule:
+// You define a schedule using a cron expression, which consists of five fields representing:
+
+//! MINUTE, HOUR, DAY OF THE MONTH, MONTH, DAY OF THE WEEK
+
+//? EXAMPLES && EXPLANATION:
+//* 14 * * * * - Every 14 minutes
+//* 0 0 * * 0 - At midnight on every Sunday
+//* 30 3 15 * * - At 3:30 AM, on the 15th of every month
+//* 0 0 1 1 * - At midnight, on January 1st
+//* 0 * * * * - Every hour
